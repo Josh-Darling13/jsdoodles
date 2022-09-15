b59c2bb0fd023e88aad87c680cd10c1c245adc24
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-09-15 10:42:16 -0400

    first commit

---
+++ string_triangle.js
@@ -0,0 +1,15 @@
+/*
+A simple program with an out of strings 
+in the shape of a trianlge to the console. 
+*/
+
+
+const foo = "#";
+let repeatedString = "";
+let n = 0;
+
+while (n < 8) {
+  repeatedString += foo;        
+  console.log(repeatedString);
+  n++;
+};
