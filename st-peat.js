09ee6cb3e183e90af7309fe7026cc6118ffb21ba
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-11-23 21:58:49 -0500

    reliazed mulipte solutionas ot same problem.

--- st-peat.js
+++ st-peat.js
@@ -5,16 +5,22 @@ if there is no such character
 return null.
 */
 
-const testStr = "The quick brown fox jumped over the lazy dog.";    // test string with repeating letters
-// const moTestStr = "flamdo";                                      // test string no repeating letters
+const testStr = "The quick brown fox jumped over the lazy dog";    // test string with repeating letters
+// const testStr = "flamdo";                                      // test string no repeating letters
+let matchFound = false;
+
+// This has to be done with while while loops 
+// OR as a second solution split and remove 
+// search functions.
 
 const pairTester = (strTest) => {
     const orgArray = strTest.split('');
     orgArray.forEach(element => {
         console.log(element);
     });
+
+
 }
 
+
 pairTester(testStr);
-
-
