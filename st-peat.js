e20197ab048a01dc0c8dab68032f44746b0a0fa1
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-11-25 16:27:09 -0500

    added varaible for loop control conditional

--- st-peat.js
+++ st-peat.js
@@ -21,19 +21,21 @@ const pairTesterStrFunc = (strTest) => {
 }
 
 // pairTesterWhlFunc(strTest){
-    const orgArray = strTest.split('');                                 // Make the string into an array.
-    const copyArray = orgArray;                                         // Copy the original array.
-    const arrayLenght = orgArray.length;                                // Finds the Length of the array.
-    let countOrgArray = 0;                                              // Counter variable set to 0 for the original array.
-    let countCopyArray = 0;                                             // Counter variable set to 0 for the original array.
+//     const orgArray = strTest.split('');                                 // Make the string into an array.
+//     const copyArray = orgArray;                                         // Copy the original array.
+//     const arrayLenght = orgArray.length;                                // Finds the Length of the array.
+//     let countOrgArray = 0;                                              // Counter variable set to 0 for the original array.
+//     let countCopyArray = 0;                                             // Counter variable set to 0 for the original array.
+//     let loopBreak = false;                                               // variable for breaking out of loops
 
-    while(){
-        while(){}       //if the elements are the same but the array keys are different return the letter
-                        // else if both counters are equal to the length of the array and return null
-    }                   
+//     while(){
+//         while(){}       //if the elements are the same but the array keys are different return the letter set loopStop to true
+//                         // else if both counters are equal to the length of the array and return null
+//     }
 
-
-
+//     if (loopBreak === true){
+//         break;
+//     }
 // }
 
 // pairTesterWhlFunc(testStr);
