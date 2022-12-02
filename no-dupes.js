5a7c712a4ccb5526fb29a8e75fe31fe7d7c0d29c
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-12-01 20:16:38 -0500

    created first part of number search

--- no-dupes.js
+++ no-dupes.js
@@ -17,55 +17,73 @@ fun to when writing the program.
 
 // array maker...
 // makes and array filled with random numbers
-const arrayThisLong = Math.floor(Math.random() * 100) + 20;
+// const arrayThisLong = Math.floor(Math.random() * 100) + 20;
 
-let n = 0;
-let testArray = [];
+// let n = 0;
+// let testArray = [];
 
-while(n < arrayThisLong){
-    let randoInts = Math.floor(Math.random() * 1000);
-    testArray[n] = randoInts;
-    n++;
-}
+// while(n < arrayThisLong){
+//     let randoInts = Math.floor(Math.random() * 1000);
+//     testArray[n] = randoInts;
+//     n++;
+// }
 
-// console.log(testArray);
+// // console.log(testArray);
 
-const keyValues = Math.round(testArray.length/2);                       // halves the amount of potential random searching
-const keyLost = Math.floor(Math.random() * keyValues) + 3;              // randomly picks times elements will be duplicated.
-// console.log(keyLost);
-let stolenElements = [];
+// const keyValues = Math.round(testArray.length/2);                       // halves the amount of potential random searching
+// const keyLost = Math.floor(Math.random() * keyValues) + 3;              // randomly picks times elements will be duplicated.
+// // console.log(keyLost);
+// let stolenElements = [];
 
-let k = 0;
+// let k = 0;
 
-while (k < keyLost){
-    let keyFound = Math.floor(Math.random() * testArray.length);
-    stolenElements[k] = testArray[keyFound];
-    k++;
-}
+// while (k < keyLost){
+//     let keyFound = Math.floor(Math.random() * testArray.length);
+//     stolenElements[k] = testArray[keyFound];
+//     k++;
+// }
 
-console.log(`stolen elements: ${stolenElements} \n `);
+// console.log(`stolen elements: ${stolenElements} \n `);
 
 
-let i = 0;
+// let i = 0;
 
-while (i < stolenElements.length){
-    let keyFound = Math.floor(Math.random() * testArray.length);
-    testArray.splice(keyFound, 0, stolenElements[i]);
-    i++;
-}
+// while (i < stolenElements.length){
+//     let keyFound = Math.floor(Math.random() * testArray.length);
+//     testArray.splice(keyFound, 0, stolenElements[i]);
+//     i++;
+// }
 
-console.log(`stolen elements in test array: ${testArray} \n `);
+// console.log(`stolen elements in test array: ${testArray} \n `);
 
 
 
 
 
 // Static array for building on later
-// let orgArray = [
-//     150, 523, 897, 918, 974, 628, 759, 709,
-//      64, 829, 775, 262, 880, 168, 784, 974,
-//     903, 669, 741,  15, 713, 138, 625, 964,
-//     620, 785, 398, 926, 351, 292,  47, 472,
-//     194, 525, 457, 469, 918, 116,  28, 537,
-//     893, 609, 827
-//   ];
+let orgArray = [
+    150, 666, 523, 897, 918, 974, 628, 759, 
+     64, 829, 775, 262, 880, 168, 784, 974,
+    903, 669, 741,  15, 713, 138, 625, 964,
+    620, 785, 398, 926, 351, 292,  47, 472,
+    194, 525, 457, 469, 918, 116,  28, 537,
+    893, 609, 827, 666, 709
+  ];
+
+const copyArray = [...orgArray];
+
+for (let element in orgArray){
+    // console.log(orgArray[element]);
+    let searchFor = copyArray.shift();
+    console.log(searchFor);
+    let whereInArray = orgArray.indexOf(searchFor);
+    console.log(whereInArray);
+    if (whereInArray !== -1){
+        orgArray.splice(whereInArray,1);
+        console.log("made it here");
+    }
+}
+// 918 974
+console.log(orgArray);
+console.log(orgArray[10]);
+console.log(orgArray[32]);
